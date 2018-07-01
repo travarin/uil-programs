7927d8722447e02d27b218e27e8aac6643a50cd6
Travarin <langstont583@gmail.com>
2018-07-01 18:24:51 -0500

    Added solution for Problem 5: Family Portrait

---
+++ src/com/github/district22012/Family.java
@@ -0,0 +1,50 @@
+package com.github.district22012;
+/*
+ * Travis Langston
+ * 
+ * 12/2/2017
+ * District 2 2012 Problem 5
+ * This program will read in a picture in portrait layout, and rotate it 90 degrees to landscape.
+ */
+import java.util.*;
+import java.io.*;
+public class Family {
+
+	public static void main(String[] args) {
+		Scanner rdIn;
+		try
+		{
+			rdIn = new Scanner(new File("com/github/district22012/family.dat"));
+			int pictures = rdIn.nextInt();
+			for ( int i = 0; i < pictures; i++ )
+			{
+				char[][] pixels = new char[18][12];
+				for ( int r = 0; r < 18; r++ )
+				{
+					String line = rdIn.next();
+					for ( int c = 0; c < 12; c++ )
+					{
+						pixels[r][c] = line.charAt(c);
+					}
+				}
+				
+				for ( int c = 0; c < 12; c++ )
+				{
+					for ( int r = 17; r > -1; r-- )
+					{
+						System.out.print(pixels[r][c]);
+					}
+					System.out.println();
+				}
+				System.out.println();
+			}
+			rdIn.close();
+		}
+		catch ( IOException e )
+		{
+			System.err.println("Error: " + e );
+		}
+
+	}
+
+}
