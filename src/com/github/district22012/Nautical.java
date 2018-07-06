f64c5ba3647461ad9f88bd151df2e09269512b21
Travarin <langstont583@gmail.com>
2018-07-05 21:01:01 -0500

    Added solution for Problem 9: Nautical Miles

---
+++ src/com/github/district22012/Nautical.java
@@ -0,0 +1,37 @@
+package com.github.district22012;
+
+/**
+ * Travis Langston
+ *
+ * 7/5/2018
+ * District 2 2012 Problem 9
+ * This program will read in a list of distances a ship has traveled in nautical miles,
+ * and convert them to English miles. (1 nautical mile = 1.15 English miles)
+ */
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.File;
+public class Nautical
+{
+    public static void main(String[] args)
+    {
+        Scanner rdIn;
+        try
+        {
+            rdIn = new Scanner(new File("com/github/district22012/nautical.dat"));
+            int distances = rdIn.nextInt();
+            for ( int i = 0; i < distances; i++ )
+            {
+                double distance, english;
+                distance = rdIn.nextInt();
+                english  = distance * 1.15;
+                System.out.printf("%.1f\n", english);
+            }
+            rdIn.close();
+        }
+        catch ( IOException e )
+        {
+            System.err.println("Error: " + e);
+        }
+    }
+}
